11db24257d1857ba6cfa916d044d305748c9429b
sean burke <s.burke15@gmail.com>
2018-03-24 13:11:30 -0400

    modified:   run.py

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ from flask_app import app as app2
 app = DispatcherMiddleware(server, {'/myflaskapp': app2})
 
 if __name__ == '__main__':
-    run_simple('localhost', 5000, app, use_reloader=True)
+    run_simple('127.0.0.1', 5000, app, use_reloader=True)
