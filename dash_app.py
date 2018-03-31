699bdc5dfffa88df0e0fa14f765cba22d767ca9e
sean burke <s.burke15@gmail.com>
2018-03-31 15:14:20 -0400

    added tabs to dash

--- dash_app.py
+++ dash_app.py
@@ -1,23 +1,120 @@
 import dash
 import dash_core_components as dcc 
 import dash_html_components as html 
+from dash.dependencies import Input, Output
 from server import server
 
 app = dash.Dash(name='mydash', sharing=True, server=server, url_base_pathname='/mydash')
 
-app.layout = html.Div(children=[
-    html.H1(children='Dash Test'),
-    dcc.Graph(
-        id='example',
-        figure={
-            'data': [
-                {'x': [1, 2, 3, 4, 5], 'y': [9, 6, 2, 1, 5], 'type': 'line', 'name': 'Boats'},
-                {'x': [1, 2, 3, 4, 5], 'y': [8, 7, 2, 7, 3], 'type': 'bar', 'name': 'Cars'},
-            ],
-            'layout': {
-                'title': 'Basic Dash Example'
-            }
-        }
-    )
-])
+vertical = True
 
+if not vertical:
+    app.layout = html.Div([
+        dcc.Tabs(
+            tabs=[
+                {'label': 'Market Value', 'value': 1},
+                {'label': 'Usage Over Time', 'value': 2},
+                {'label': 'Predictions', 'value': 3},
+                {'label': 'Target Pricing', 'value': 4},
+            ],
+            value=3,
+            id='tabs',
+            vertical=vertical
+        ),
+        html.Div(id='tab-output')
+    ], style={
+        'width': '80%',
+        'fontFamily': 'Sans-Serif',
+        'margin-left': 'auto',
+        'margin-right': 'auto'
+    })
+
+else:
+    app.layout = html.Div([dcc.Dropdown(
+        id='my-dropdown',
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': 'Montreal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value='NYC'
+    ),html.P(''),
+        html.Div(
+            dcc.Tabs(
+                tabs=[
+                    {'label': 'Market Value', 'value': 1},
+                    {'label': 'Usage Over Time', 'value': 2},
+                    {'label': 'Predictions', 'value': 3},
+                    {'label': 'Target Pricing', 'value': 4},
+                ],
+                value=3,
+                id='tabs',
+                vertical=vertical,
+                style={
+                    'height': '100vh',
+                    'borderRight': 'thin lightgrey solid',
+                    'textAlign': 'left'
+                }
+            ),
+            style={'width': '20%', 'float': 'left'}
+        ),
+        html.Div(
+            html.Div(id='tab-output'),
+            style={'width': '80%', 'float': 'right'}
+        ),html.H1('layout test'),
+    ], style={
+        'fontFamily': 'Sans-Serif',
+        'margin-left': 'auto',
+        'margin-right': 'auto',
+    })
+
+
+@app.callback(Output('tab-output', 'children'), [Input('tabs', 'value')])
+def display_content(value):
+    data = [
+        {
+            'x': [1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
+                  2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
+            'y': [219, 146, 112, 127, 124, 180, 236, 207, 236, 263,
+                  350, 430, 474, 526, 488, 537, 500, 439],
+            'name': 'Rest of world',
+            'marker': {
+                'color': 'rgb(55, 83, 109)'
+            },
+            'type': ['bar', 'scatter', 'box'][int(value) % 3]
+        },
+        {
+            'x': [1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
+                  2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
+            'y': [16, 13, 10, 11, 28, 37, 43, 55, 56, 88, 105, 156, 270,
+                  299, 340, 403, 549, 499],
+            'name': 'China',
+            'marker': {
+                'color': 'rgb(26, 118, 255)'
+            },
+            'type': ['bar', 'scatter', 'box'][int(value) % 3]
+        }
+    ]
+
+    return html.Div([
+        dcc.Graph(
+            id='graph',
+            figure={
+                'data': data,
+                'layout': {
+                    'margin': {
+                        'l': 30,
+                        'r': 0,
+                        'b': 30,
+                        't': 0
+                    },
+                    'legend': {'x': 0, 'y': 1}
+                }
+            }
+        ),
+    html.Div(children=[html.H1(['first','second','third','fourth'][int(value) %3], style={'textAlign': 'center'}),
+    html.P('This is paragraph')])
+    ])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
